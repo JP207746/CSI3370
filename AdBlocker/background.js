7187a1df5d6354be8d32b79e4f25589ee04b2194
Joey Pio <54558640+JP207746@users.noreply.github.com>
2021-11-04 16:09:27 -0400

    Worked on Blob

--- AdBlocker/background.js
+++ AdBlocker/background.js
@@ -1,7 +1,9 @@
-file_in = open("links.rtf", "adblocker");
-
+let blockList = new FileReader("links.rtf");
+//var MyBlob = new Blob([file_in], {type : 'text/plain'});
+let file = new file([""], {type : 'text/plain'});
+file_in = blockList.readAsDataURL(file);
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) { return { cancel: true }},
-    { urls: defaultFilters },
+    { urls: blockList },
     ["blocking"]
 )
