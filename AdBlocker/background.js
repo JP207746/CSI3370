f452056cd146fc0796ede18a8f390c49f876813b
apetron <77649959+apetron@users.noreply.github.com>
2021-11-06 10:48:09 -0400

    Added reading the file
    The code now (mostly) reads the links.rtf file. I think we might need to find another way to do it though.

--- AdBlocker/background.js
+++ AdBlocker/background.js
@@ -1,9 +1,26 @@
-let blockList = new FileReader("links.rtf");
-//var MyBlob = new Blob([file_in], {type : 'text/plain'});
-let file = new file([""], {type : 'text/plain'});
-file_in = blockList.readAsDataURL(file);
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { return { cancel: true }},
-    { urls: blockList },
+function readTextFile(callback)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "links.rtf", false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+				var array = allText.split('\\');
+				alert(array);
+                return allText;
+            }
+        }
+    }
+    rawFile.send(null);
+	return null;
+}
+
+chrome.webRequest.onBeforeRequest.addListener(	
+	function(details) { return { cancel: true }},
+    { urls: readTextFile() },
     ["blocking"]
 )
